da4856e0a1ec2a75592d6a0464c7d46810fa1009
Solomon S <schinoyerem007@gmail.com>
2022-07-31 11:04:43 +0100

    configure - infra

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export: TYPEORM_CONNECTION-postgres
+export: TYPEORM_ENTITIES-./src/moudles/domain/**/*.entity.ts 
+export: TYPEORM_HOST-udapeopledb.cpszwuc48xma.us-east-1.rds.amazonaws.com
+export: TYPEORM_PORT-5432
+export: TYPEORM_USERNAME-udapeople
+export: TYPEORM_PASSWORD-!Pass4sure
+export: TYPEORM_DATABASE-udapeopledb
+export: TYPEORM_MIGRATIONS-/src/migrations/*.ts
+export: TYPEORM_MIGRATIONS_DIR-/src/migrations/
